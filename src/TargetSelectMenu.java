363888c0afb67d5ea19ea50a33a1bea02a477c5a
Alisa Lytvyn <metherat@gmail.com>
2016-04-29 10:54:15 -0400

    Added label printing to targetSelectMenu, it's not working thoorking though.

--- src/TargetSelectMenu.java
+++ src/TargetSelectMenu.java
@@ -1,4 +1,8 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -15,6 +19,7 @@ public class TargetSelectMenu extends Menu
 	//TargetMenu can only check for targets in these four locations:
 	private int[] xEntityPos = {32, 160, 512, 384};
 	private int[] yEntityPos = {192, 32, 192, 32};
+	private String selectLabel;
 	//So, bounds is just {x1, y1, x2, y2, x3, y3, x4, y4}, with smaller denominations possible
 	/**
 	 * Constructor for a TargetSelectMenu
@@ -29,12 +34,15 @@ public class TargetSelectMenu extends Menu
 			case "All":
 				setBounds(new double[][]{{xEntityPos[0], yEntityPos[0]},{xEntityPos[1], yEntityPos[1]},
 										 {xEntityPos[2], yEntityPos[2]},{xEntityPos[3], yEntityPos[3]}});
+				selectLabel = "Select a target: ";
 				break;
 			case "Enemies":
 				setBounds(new double[][]{{xEntityPos[2], yEntityPos[2]},{xEntityPos[3], yEntityPos[3]}});
+				selectLabel = "Select an enemy: ";
 				break;
 			case "Playable":
 				setBounds(new double[][]{{xEntityPos[0], yEntityPos[0]},{xEntityPos[1], yEntityPos[1]}});
+				selectLabel = "Select a teammate: ";
 				break;
 		}
 		//Set image as the red selection border
@@ -63,5 +71,23 @@ public class TargetSelectMenu extends Menu
 		{
 			g.drawImage(image, (int)bounds[i][0], (int)bounds[i][1], 128, 128, null);
 		}
+		g.setColor(Color.BLACK);
+		Font font = new Font("Arial", Font.BOLD, 32);
+		drawCenteredString(g, selectLabel, new Rectangle(0,352,672,480), font);
+	}
+	/**
+	 * Draw a string in the center of a rectangle
+	 * @param g: the graphics object in which to draw the string
+	 * @param text: string to be drawn
+	 * @param rect: rectangle to draw the string in
+	 * @param font: font of the string
+	 */
+	public void drawCenteredString(Graphics g, String text, Rectangle rect, Font font) 
+	{
+	    FontMetrics metrics = g.getFontMetrics(font);
+	    int x = (rect.width - metrics.stringWidth(text)) / 2 + rect.x;
+	    int y = ((rect.height) / 2) + rect.y + metrics.getAscent() / 2;
+	    g.setFont(font);
+	    g.drawString(text, x, y);
 	}
 }
